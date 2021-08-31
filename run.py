ef5ccb492f1b8ba46b9d0bfe40687d89307d9b07
skaylan <lucas_7tv@hotmail.com>
2021-08-31 18:29:12 -0300

    small changes

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from app import app
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+port = os.getenv('PORT')
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=port)
